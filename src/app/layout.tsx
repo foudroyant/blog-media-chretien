447c3f7edf3737a85af7fb67eb9933fbf2183b63
Foudroyant <stephanebazebibouta@gmail.com>
2025-12-31 20:25:19 +0100

    ajout de vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import cn from "classnames";
 import { ThemeSwitcher } from "./_components/theme-switcher";
-
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -65,6 +65,7 @@ export default function RootLayout({
         <Navbar />
         <div className="min-h-screen">{children}</div>
         <Footer />
+        <Analytics/>
       </body>
     </html>
   );
